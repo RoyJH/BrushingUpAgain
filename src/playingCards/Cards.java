2317cf1e2960aa002d014cbe03168f60b86e85c1
Roy Harris <Zithlos@gmail.com>
2020-03-06 21:44:20 -0600

    Add Card elements and start Deck Population

--- src/playingCards/Cards.java
+++ src/playingCards/Cards.java
@@ -1,5 +1,49 @@
 package playingCards;
 
 public class Cards {
-
+	private int value;
+	private char face;
+	private String name;
+	private String suit;
+	
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public char getFace() {
+		return face;
+	}
+	public void setFace(char face) {
+		this.face = face;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	
+	Cards(int value, char face, String name, String suit) {
+		this.value = value;
+		this.face = face;
+		this.name = name;
+		this.suit = suit;
+	}
+	public Cards makeCards() {
+		Cards card = new Cards();
+		card.getValue();
+		card.getFace();
+		card.getName();
+		card.getSuit();
+		return card;
+	}
+	
 }
--- src/playingCards/Deck.java
+++ src/playingCards/Deck.java
@@ -1,10 +1,32 @@
 package playingCards;
 
-public class Deck {
+public class Deck extends Cards{
+	private static final Object[] Deck = null;
+
+	Deck(int value, char face, String name, String suit) {
+		super(value, face, name, suit);
+		// TODO Auto-generated constructor stub
+	}
 	private int value;
 	private char face;
 	private String name;
 	private String suit;
+	private int id;
 	
-	
+	public void populate() {
+		Deck deck = new Deck();
+		for (int i = 0; i < 52; i++) {
+			if(i < 13 ) {suit = "club";}
+			else if (i < 52) {suit = "spade";}
+			
+			Cards(value, face, name, suit);
+			}System.out.println(Cards.suit);
+		}
+
+	public void shuffle() {
+		
+	}
+	public void cut() {
+		
+	}
 }
