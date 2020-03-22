dec1ddca3a32df1997fda657646f668177b57f82
Roy Harris <Zithlos@gmail.com>
2020-03-22 15:09:14 -0500

    Card creation successful. Cleaned up code and removed unused segments.

--- src/playingCards/Play.java
+++ src/playingCards/Play.java
@@ -1,12 +1,13 @@
 package playingCards;
-
 public class Play {
-
 	public static void main(String[] args) {
-		Cards card[] = new Cards[51];
-		for (int i=0;i<=51;i++) {
-			card[i].makeCards(i);
+		Cards card[] = new Cards[52];
+		for (int i=0; i<=51; i++) {
+			card[i] = new Cards();
+		}
+		for (int j=0;j<=51;j++) {
+			card[j].makeCards(j);
+			card[j].print();
 		}
 	}
-	
 }
